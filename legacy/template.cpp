229cbf3f08a3aed6b5dca18ec0abf6ff5c2d786d
joel chacon <joel.chacon@oracle.com>
2024-10-12 17:26:06 -0600

    legacy template

---
+++ legacy/template.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+
+#define io_optimization ios_base::sync_with_stdio(0);cin.tie(0);
+typedef pair<int, int>             pii;
+typedef pair<long long, int>       pli;
+typedef pair<pii, int>              piii;
+typedef pair<long long, long long> pll;
+typedef pair<pll, long long>        plll;
+typedef vector<int>                vi;
+typedef vector<bool>                vb;
+typedef vector<vector<int>>        vvi;
+typedef vector<long long>          vl;
+typedef vector<vector<long long>>  vvl;
+typedef vector<pii>                vpii;
+typedef vector<piii>               vpiii;
+typedef vector<pli>                vpli;
+typedef vector<pll>                vpll;
+typedef vector<plll>               vplll;
+typedef vector<vector<plll>>       vvplll;
+typedef vector<vector<pll>>        vvpll;
+typedef vector<vector<piii>>       vvpiii;
+typedef vector<vector<pii>>        vvpii;
+typedef vector<vector<pli>>        vvpli;
+
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define data data_
+#define endl "\n"
+#define isOn(S, j) (S & (1ll << (j)))
+#define setBit(S, j) (S |= (1ll << (j)))
+#define clearBit(S, j) (S &= ~(1ll << (j)))
+#define toggleBit(S, j) (S ^= (1ll << (j)))
+#define bzero(b,len) (memset((b), '\0', (len)), (void) 0)
+#define all(x) x.begin(),x.end()
+#define sz(x) x.size()
+#define dbg(v) cout << "Line(" << __LINE__ << ") -> " << #v << " = " << (v) << endl
+
+int main(){
+	io_optimization;
+	int T; cin >> T;
+	for (int i = 0; i < T; i++){
+	}
+	return 0;
+}
+
